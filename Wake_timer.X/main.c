2cb6e70a5029a9dbacfc8195d7a5f98f59b581f5
Rodrigo Caldeira <rodishumy@gmail.com>
2025-09-24 17:09:28 +0100

    Timer works
    Timer code has been tested and works.

--- Wake_timer.X/main.c
+++ Wake_timer.X/main.c
@@ -17,10 +17,11 @@
 
 void timer1_setup(uint16_t period)
 {
-    T1CON = 0x00;   //Stops the Timer1 and reset control reg.
+    T1CON = 0x00;   //Stops the Timer1 and reset control register
     TMR1 = 0x00;    //Clear contents of the timer register
     PR1 = period;   //Load the timer with the target period
     
+    T1CONbits.TCKPS = 0b11; //Set pre-scaler 1:256
     IPC0bits.T1IP = 0b111;  //Define the Timer1 interrupt priority (in this case highest priority)
     IFS0bits.T1IF = 0;      //Clear Timer1 interrupt flag
     IEC0bits.T1IE = 1;      //Enable the timer interrupt
@@ -33,19 +34,19 @@ void timer1_setup(uint16_t period)
 
 void __attribute__((__interrupt__, __shadow__)) _T1Interrupt(void)
 {
-    IFS0bits.T1IF = 0;  //Reset Timer1 interrupt flag
     TMR1 = 0x00;        //Reset the timer, restarting count
-    LATBbits.LATB3 = ~LATBbits.LATB3;   //Toggle LED
+    IFS0bits.T1IF = 0;  //Reset Timer1 interrupt flag
+    LATBbits.LATB6 = ~LATBbits.LATB6;   //Toggle LED
 }
 
 int main(void) {
-    ANSELBbits.ANSB3 = 0;
-    TRISBbits.TRISB3 = 0;
-    LATBbits.LATB3 = 0;
+    //ANSELBbits.ANSB6 = 0;
+    TRISBbits.TRISB6 = 0;
+    LATBbits.LATB6 = 0;
     
-    uint16_t timer_period_ms = 1000;
+    uint32_t timer_period_ms = 1000;
             
-    timer1_setup(timer_period_ms*FCY/1000);  // Check if conversion is correct
+    timer1_setup((timer_period_ms/1000)*FCY/256);  //Check if conversion is correct
     
     while(1)
     {
