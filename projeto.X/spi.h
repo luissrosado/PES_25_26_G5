a8e30a2d83fe04f35fd771679ac2e4d36fad02e8
garciax4 <140191117+garciax4@users.noreply.github.com>
2025-10-01 16:21:51 +0000

    criação do ficheiro projeto

---
+++ projeto.X/spi.h
@@ -0,0 +1,14 @@
+#ifndef
+
+#define SPI_SS_ANS       ANSELBbits.ANSB3 
+#define SPI_SS_TRIS      TRISBbits.TRISB3
+#define SPI_SS_PORT      PORTBbits.RB3
+#define LED_TRIS         TRISBbits.TRISB14
+#define LED_LAT          LATBbits.LATB14
+
+
+void SPI1Init(void);
+void __attribute__((interrupt, no_auto_psv)) _SPI1RXInterrupt(void);
+
+
+#endif
