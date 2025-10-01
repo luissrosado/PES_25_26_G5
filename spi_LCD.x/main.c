fa294499bccf184278a88905cc9131a2d7dc646c
garciax4 <140191117+garciax4@users.noreply.github.com>
2025-10-01 17:27:29 +0000

    spi master com ecra

---
+++ spi_LCD.x/main.c
@@ -0,0 +1,189 @@
+/*******************************************************************************
+Copyright 2016 Microchip Technology Inc. (www.microchip.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "bsp/adc.h"
+#include "bsp/lcd.h"
+#include "bsp/timer_1ms.h"
+#include "bsp/buttons.h"
+#include "bsp/leds.h"
+#include "bsp/rtcc.h"
+#include "p24FJ1024GB610.h"
+ 
+#define FOSC 8000000UL
+#define FCY FOSC/2
+#include "io_mapping.h"
+#include <libpic30.h>
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: File Scope Variables and Functions
+// *****************************************************************************
+// *****************************************************************************
+extern void SYS_Initialize ( void ) ;
+static void BlinkAliveEventHandler( void );
+static void ScreenUpdateEventHandler( void );
+
+static RTCC_DATETIME time;
+static RTCC_DATETIME lastTime = {0};
+static volatile bool toggleBlinkAlive = false;
+static volatile bool allowScreenUpdate = true;
+
+#define MEMCMP_VALUES_IDENTICAL 0
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+
+void spi_init()
+{
+   SPI1CON1Lbits.SPIEN = 0;      // disable the spi peripheral
+   IFS0bits.SPI1IF = 0;         //  Clear spi interrupt flag
+   IEC0bits.SPI1IE = 0;         //  Disable spi interrupt flag
+   SPI1CON1bits.DISSCK = 0;      // Enable SCK signal generation
+   SPI1CON1bits.DISSDO = 0;      // Enable sdo signal generation
+   SPI1CON1bits.MODE16 = 1;   // Set 16-bit mode
+   SPI1CON1bits.SMP = 0;         // Input is sample at mid-bit
+   SPI1CON1bits.CKP = 0;         // clock is low when idle
+   SPI1CON1bits.CKE = 1;          // Data changes on the falling edge (because idle is on low if ckp = 1 changes this bit)
+   SPI1CON1bits.MSTEN = 1;       // Define device as master
+   SPI1BRGLbits.BRG = 4;                 // fcy/BRG = freq of spi
+   SPI1CON2 = 0;                 // disable frame mode
+   RPINR20bits.SDI1R = 26;        // miso config pin to rp26 P11
+   RPOR9bits.RP19R = 7;           // mosi config pin to rp19 P12
+   RPOR10bits.RP21R = 8;           // clock config pin to rp21 P10
+   
+   // chip select pin RB1  P24
+   ANSELBbits.ANSB1 = 0;
+   TRISBbits.TRISB1 = 0;
+   
+   ANSELGbits.ANSG7 = 0;
+   ANSELGbits.ANSG8 = 0;
+
+   
+   SPI1CON1Lbits.SPIEN = 1;      // Enable the spi peripheral
+   
+   
+   
+
+}
+/*
+void spi_com(uint16_t *data, uint16_t *rx_data, uint16_t length)
+{
+    LATBbits.LATB1 = 0;
+    __delay_us(5);// select slave
+    //for(uint16_t i = 0; i < length; i++)
+    //{
+    SPI1BUFL  = data[1];                 // write to buffer for Transmit
+    while(!SPI1STATLbits.SPIRBF);       // Wait until tx is completed
+    rx_data[1] = SPI1BUFL;               // read the received value 
+    //}
+    LATBbits.LATB1 = 1;
+}*/
+
+void spi_com(uint16_t *data, uint16_t *rx_data, uint16_t length)
+{
+    LATBbits.LATB1 = 0;   // CS baixo
+    __delay_us(5);
+
+    for (uint16_t i = 0; i < length - 1; i++) {
+        SPI1BUFL = data[i];               // envia no MOSI
+        while(!SPI1STATLbits.SPIRBF);     // espera terminar
+        rx_data[i + 1] = SPI1BUFL;            // lê MISO
+    }
+
+    LATBbits.LATB1 = 1;   // CS alto
+}
+
+
+
+
+int main ( void )
+{
+    uint16_t adcResult;
+    uint16_t lastAdcResult = 0xFFFF;
+    
+    /* Call the System Initialize routine*/
+    SYS_Initialize ( );
+    
+    /* To determine how the LED and Buttons are mapped to the actual board
+     * features, please see io_mapping.h. */
+    LED_Enable ( LED_BLINK_ALIVE );
+    LED_Enable ( LED_BUTTON_PRESSED );
+
+    BUTTON_Enable ( BUTTON_DEMO );
+    
+    /* Get a timer event once every 100ms for the blink alive. */
+    TIMER_SetConfiguration ( TIMER_CONFIGURATION_1MS );
+    TIMER_RequestTick( &BlinkAliveEventHandler, 500 );
+    TIMER_RequestTick( &ScreenUpdateEventHandler, 170 );
+    
+    /* The TIMER_1MS configuration should come before the RTCC initialization as
+     * there are some processor modules that require the TIMER_1MS module to be
+     * configured before the RTCC module, as the RTCC module is emulated using
+     * the TIMER_1MS module. */
+    time.bcdFormat = false;
+    lastTime.bcdFormat = false;
+    RTCC_BuildTimeGet( &time );
+    RTCC_Initialize( &time );
+    memset(&lastTime,0,sizeof(lastTime)); 
+
+    ADC_SetConfiguration ( ADC_CONFIGURATION_DEFAULT );
+    ADC_ChannelEnable ( ADC_CHANNEL_POTENTIOMETER );
+    
+    /* Clear the screen */
+    printf( "\f" );   
+    
+    uint16_t tx_data[4] = {0xFFFF, 0xFFFF, 0x1234,  0x4567};
+    uint16_t rx_data[4];
+    
+    spi_init();
+    printf("olaaaaaaa");
+    while(1)
+    {
+        
+        printf("1");
+        LATBbits.LATB1 = 1;
+        __delay_ms(1000);
+        spi_com(tx_data, rx_data, 4);
+        printf("\f");
+        for (uint16_t i = 0; i < 3; i++){
+            printf("%04X %d", rx_data[i+], i+);
+            __delay_ms(1000);
+            printf("\f");
+        }
+    }
+    
+}
+
+static void BlinkAliveEventHandler(void)
+{    
+    toggleBlinkAlive = true;
+}
+
+static void ScreenUpdateEventHandler(void)
+{
+    allowScreenUpdate = true;
+}
+
---
+++ spi_LCD.x/nbproject/Makefile-default.mk
@@ -0,0 +1,249 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=bsp/adc.c bsp/rtcc.c bsp/buttons.c bsp/lcd.c bsp/leds.c bsp/lcd_printf.c bsp/timer_1ms.c main.c system.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/bsp/adc.o ${OBJECTDIR}/bsp/rtcc.o ${OBJECTDIR}/bsp/buttons.o ${OBJECTDIR}/bsp/lcd.o ${OBJECTDIR}/bsp/leds.o ${OBJECTDIR}/bsp/lcd_printf.o ${OBJECTDIR}/bsp/timer_1ms.o ${OBJECTDIR}/main.o ${OBJECTDIR}/system.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/bsp/adc.o.d ${OBJECTDIR}/bsp/rtcc.o.d ${OBJECTDIR}/bsp/buttons.o.d ${OBJECTDIR}/bsp/lcd.o.d ${OBJECTDIR}/bsp/leds.o.d ${OBJECTDIR}/bsp/lcd_printf.o.d ${OBJECTDIR}/bsp/timer_1ms.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/system.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/bsp/adc.o ${OBJECTDIR}/bsp/rtcc.o ${OBJECTDIR}/bsp/buttons.o ${OBJECTDIR}/bsp/lcd.o ${OBJECTDIR}/bsp/leds.o ${OBJECTDIR}/bsp/lcd_printf.o ${OBJECTDIR}/bsp/timer_1ms.o ${OBJECTDIR}/main.o ${OBJECTDIR}/system.o
+
+# Source Files
+SOURCEFILES=bsp/adc.c bsp/rtcc.c bsp/buttons.c bsp/lcd.c bsp/leds.c bsp/lcd_printf.c bsp/timer_1ms.c main.c system.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=24FJ1024GB610
+MP_LINKER_FILE_OPTION=,--script=p24FJ1024GB610.gld
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/bsp/adc.o: bsp/adc.c  .generated_files/flags/default/da4336b4205e59933d3573b897db7c737769f503 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/adc.o.d 
+	@${RM} ${OBJECTDIR}/bsp/adc.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/adc.c  -o ${OBJECTDIR}/bsp/adc.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/adc.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/rtcc.o: bsp/rtcc.c  .generated_files/flags/default/5e58866d57be97bda9ffdf15f8e67297b4284bec .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/rtcc.o.d 
+	@${RM} ${OBJECTDIR}/bsp/rtcc.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/rtcc.c  -o ${OBJECTDIR}/bsp/rtcc.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/rtcc.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/buttons.o: bsp/buttons.c  .generated_files/flags/default/3780f34e0a7bec0ed39d8fc478528c2fa1eeba7b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/buttons.o.d 
+	@${RM} ${OBJECTDIR}/bsp/buttons.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/buttons.c  -o ${OBJECTDIR}/bsp/buttons.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/buttons.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/lcd.o: bsp/lcd.c  .generated_files/flags/default/d6caeb83dc8d5a3e64bb8e76824d335e442e5880 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/lcd.o.d 
+	@${RM} ${OBJECTDIR}/bsp/lcd.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/lcd.c  -o ${OBJECTDIR}/bsp/lcd.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/lcd.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/leds.o: bsp/leds.c  .generated_files/flags/default/facc299492bbc0a69c13fb00007911b6e752364e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/leds.o.d 
+	@${RM} ${OBJECTDIR}/bsp/leds.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/leds.c  -o ${OBJECTDIR}/bsp/leds.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/leds.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/lcd_printf.o: bsp/lcd_printf.c  .generated_files/flags/default/de39dc1a031fbda45c1febdec1daf77f51fd265 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/lcd_printf.o.d 
+	@${RM} ${OBJECTDIR}/bsp/lcd_printf.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/lcd_printf.c  -o ${OBJECTDIR}/bsp/lcd_printf.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/lcd_printf.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/timer_1ms.o: bsp/timer_1ms.c  .generated_files/flags/default/a0cc485ed06c32a82f652fec9f09330120cc089e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/timer_1ms.o.d 
+	@${RM} ${OBJECTDIR}/bsp/timer_1ms.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/timer_1ms.c  -o ${OBJECTDIR}/bsp/timer_1ms.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/timer_1ms.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/37ee9ed7e1c6e1b396f62873bb8442a2ae09df83 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/system.o: system.c  .generated_files/flags/default/6423184f6edd6bc7df40e9796f0acaf758adc213 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/system.o.d 
+	@${RM} ${OBJECTDIR}/system.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  system.c  -o ${OBJECTDIR}/system.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/system.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+else
+${OBJECTDIR}/bsp/adc.o: bsp/adc.c  .generated_files/flags/default/20e75c1edd75e00ed396244f43062ef51a97fcb2 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/adc.o.d 
+	@${RM} ${OBJECTDIR}/bsp/adc.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/adc.c  -o ${OBJECTDIR}/bsp/adc.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/adc.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/rtcc.o: bsp/rtcc.c  .generated_files/flags/default/412bdcca313537b7906862d86d2f73579866f80a .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/rtcc.o.d 
+	@${RM} ${OBJECTDIR}/bsp/rtcc.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/rtcc.c  -o ${OBJECTDIR}/bsp/rtcc.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/rtcc.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/buttons.o: bsp/buttons.c  .generated_files/flags/default/12ad9d5cd6ff5ebe7c053aeade5c436cc9aae0c4 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/buttons.o.d 
+	@${RM} ${OBJECTDIR}/bsp/buttons.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/buttons.c  -o ${OBJECTDIR}/bsp/buttons.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/buttons.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/lcd.o: bsp/lcd.c  .generated_files/flags/default/76bce441376b0709ac01f4465ccc4a98f66e4645 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/lcd.o.d 
+	@${RM} ${OBJECTDIR}/bsp/lcd.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/lcd.c  -o ${OBJECTDIR}/bsp/lcd.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/lcd.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/leds.o: bsp/leds.c  .generated_files/flags/default/b1300a335202332a16ba863f07a60d3885946339 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/leds.o.d 
+	@${RM} ${OBJECTDIR}/bsp/leds.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/leds.c  -o ${OBJECTDIR}/bsp/leds.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/leds.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/lcd_printf.o: bsp/lcd_printf.c  .generated_files/flags/default/1fc987c4781a17598e0a57b6eaff1c59f6239549 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/lcd_printf.o.d 
+	@${RM} ${OBJECTDIR}/bsp/lcd_printf.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/lcd_printf.c  -o ${OBJECTDIR}/bsp/lcd_printf.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/lcd_printf.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/bsp/timer_1ms.o: bsp/timer_1ms.c  .generated_files/flags/default/37171ca8b0f27b59136408fee8034baa094656d8 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/bsp" 
+	@${RM} ${OBJECTDIR}/bsp/timer_1ms.o.d 
+	@${RM} ${OBJECTDIR}/bsp/timer_1ms.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  bsp/timer_1ms.c  -o ${OBJECTDIR}/bsp/timer_1ms.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/bsp/timer_1ms.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/f2d5702e5580a78e7414fae4b6a830e36dd85753 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+${OBJECTDIR}/system.o: system.c  .generated_files/flags/default/7b677df4804725750c0f7980bf828d32100b5e99 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/system.o.d 
+	@${RM} ${OBJECTDIR}/system.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  system.c  -o ${OBJECTDIR}/system.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/system.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off   
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x800:0x81B -mreserve=data@0x81C:0x81D -mreserve=data@0x81E:0x81F -mreserve=data@0x820:0x821 -mreserve=data@0x822:0x823 -mreserve=data@0x824:0x827 -mreserve=data@0x82A:0x84F   -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_PK3=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--no-isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  
+	
+else
+${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o ${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--no-isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,--report-mem,--memorysummary,${DISTDIR}/memoryfile.xml$(MP_EXTRA_LD_POST)  
+	${MP_CC_DIR}\\xc16-bin2hex ${DISTDIR}/spi_LCD.x.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf   
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(wildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
